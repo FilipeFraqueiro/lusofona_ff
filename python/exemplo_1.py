a58e1b78a4dcb428e530f1dafedb94c63f0b07ed
Filipe Fraqueiro <59435851+FilipeFraqueiro@users.noreply.github.com>
2020-11-19 20:08:28 +0000

    Add files via upload

---
+++ python/exemplo_1.py
@@ -0,0 +1,34 @@
+a = 30
+b = 20
+
+print(isinstance(a, int))
+c = a + b
+print(c)
+
+c = b > a
+print(c)
+
+c = a and b
+print(c)
+# print(bin(c))
+
+c = a is b
+print(c)
+
+print("#############################")
+
+a = "Ola"
+b = "ola"
+
+c = a + b
+print(c)
+
+c = b > a
+print(c)
+
+c = a and b
+print(c)
+# print(bin(c))
+
+c = a is b
+print(c)
